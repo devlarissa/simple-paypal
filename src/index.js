e9d0af30bb79aa962decf6fd523107b16d012288
Larissa Ramos <devlarissasr@gmail.com>
2021-07-04 12:35:25 -0300

    implementaçao paypal

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState, useEffect, useRef} from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function App() {
+  const [paid, setPaid] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  let paypalRef = useRef();
+
+  const product = {
+    price: 10.00,
+    description: "Test Paypal",
+  }
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    const id = "o client id gerado pelo paypal vai aqui"
+    script.src = "https://www.paypal.com/sdk/js?currency=BRL&client-id=${id}";
+
+    script.addEventListener("load", () => setLoaded(true));
+
+    document.body.appendChild(script);
+
+    if (loaded){
+      function loadButtonAndLogicAboutPayment(){
+        setTimeout(() => {
+          window.paypal
+            .Buttons({
+              creatOrder: (data, actions) =>{
+                return actions.order.create({
+                  purchase_units: [{
+                    description: product.description,
+                    amount: {
+                      curency_code: "BRL",
+                      value: product.price
+                    }
+                  }]
+                });
+              },
+              onApprove: async (_, actions) =>{
+                const order = await actions.order.capture();
+                setPaid(true);
+                console.log(order);
+              }
+            })
+            .render(paypalRef);
+        })
+      }
+      loadButtonAndLogicAboutPayment();
+    }
+
+  })
+
+  return (
+    <div className={"App"}>
+      {paid ? (
+        <div>
+          <h2> Compra efetuada com sucesso! </h2>
+        </div>
+      ): (
+        <>
+        <h3>{product.description} por R${product.price}</h3>
+          <div ref={v => (paypalRef = v)}/>
+        </>
+      )}
+    </div>
+  );
+}
